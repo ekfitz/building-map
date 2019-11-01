de16433c8e4f509b8b68573292ea47d25d5f0032
ekfitz <51545728+cultofsnake@users.noreply.github.com>
2019-11-01 15:53:27 -0700

    First row offices in JS

--- js/main.js
+++ js/main.js
@@ -5,12 +5,19 @@ var courtyard = document.getElementById("ctyard3");
 var a3 = document.getElementById("a3");
 var b3 = document.getElementById("b3");
 var c3 = document.getElementById("c3");
-
+var a3o1 = document.getElementById("a3o1");
+var a3o2 = document.getElementById("a3o2");
+var a3o3 = document.getElementById("a3o3");
+var a3o4 = document.getElementById("a3o4");
+var a3o5 = document.getElementById("a3o5");
+var a3o6 = document.getElementById("a3o6");
+var a3o7 = document.getElementById("a3o7");
 
 function setMobile() {
   var w = window.innerWidth
   || document.documentElement.clientWidth
   || document.body.clientWidth;
+
   if (w > 600) {
   // desktop styles
   gui.className = "guid";
@@ -20,6 +27,28 @@ function setMobile() {
   b3.className = "buildingBd";
   c3.className = "buildingC3d";
 
+  a3o1.style.left = "2%";
+  a3o1.style.top = "2.5%";
+
+  a3o2.style.left = "16%";
+  a3o2.style.top = "2.5%";
+
+  a3o3.style.left = "30%";
+  a3o3.style.top = "2.5%";
+
+  a3o4.style.left = "44%";
+  a3o4.style.top = "2.5%";
+
+  a3o5.style.left = "58%";
+  a3o5.style.top = "2.5%";
+
+  a3o6.style.left = "72%";
+  a3o6.style.top = "2.5%";
+
+  a3o7.style.left = "86%";
+  a3o7.style.top = "2.5%";
+
+
   } else {
   // mobile style
   gui.className = "guim";
@@ -28,6 +57,27 @@ function setMobile() {
   a3.className = "buildingAm";
   b3.className = "buildingBm";
   c3.className = "buildingC3m";
+
+  a3o1.style.left = "2%";
+  a3o1.style.top = "2.5%";
+
+  a3o2.style.left = "16%";
+  a3o2.style.top = "2.5%";
+
+  a3o3.style.left = "30%";
+  a3o3.style.top = "2.5%";
+
+  a3o4.style.left = "44%";
+  a3o4.style.top = "2.5%";
+
+  a3o5.style.left = "58%";
+  a3o5.style.top = "2.5%";
+
+  a3o6.style.left = "72%";
+  a3o6.style.top = "2.5%";
+
+  a3o7.style.left = "86%";
+  a3o7.style.top = "2.5%";
 
   }
 }
